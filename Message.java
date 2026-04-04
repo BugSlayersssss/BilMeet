93f6aab8e0f1c15fd3fa9901880ea2aabbc4b986
Onur Balcı <onur.balci6@gmail.com>
2026-04-04 16:46:08 +0300

    Changed the Message class to store time better and updated the methods

--- Message.java
+++ Message.java
@@ -1,19 +1,23 @@
+import java.time.LocalDateTime;
 
 public class Message {
-   private int messageId =0;
-   private String content;
-   private String sentAt;
-   private User sender;
-   public Message(String content,String sentAt,User sender){
+   private static  int messageIdCounter =1;
+   private String messageId;
+   private String content;   private User sender;
+   private LocalDateTime sentAt;
+   public Message(String content,User sender){
     this.content=content;
-    this.sentAt= sentAt;
+    this.sentAt= LocalDateTime.now();
     this.sender = sender;
-    messageId++;
-   }
-   public int getMessageId(){
-      return this.messageId;
+    this.messageId="M"+ messageIdCounter++;
    }
    public User getSender(){
       return this.sender;
+   }
+   public String getContent(){
+      return this.content;
+   }
+   public LocalDateTime getSentAt(){
+      return this.sentAt;
    }
 }
