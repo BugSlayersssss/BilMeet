5ff4103c46bf10f697bafaf09f4262531f5720ce
Onur Balcı <onur.balci6@gmail.com>
2026-04-04 16:42:57 +0300

    updated the chat class to make it work better

--- Chat.java
+++ Chat.java
@@ -1,16 +1,32 @@
+import java.util.ArrayList;
 import java.util.List;
-
 public class Chat {
-    private String chatID;
-    private String chatType;
-    private List<User> users;
+    private static int chatIdCounter = 1;
+    private boolean isPrivate;
+    private String chatId;
+    private List<User> participants;
     private List<Message> messages;
-    public Chat (String chatID, String chatType, List<User> users){
-        this.chatID= chatID;
-        this.chatType= chatType;
-        this.users= users;
+    public Chat (boolean isPrivate){
+        this.chatId="C"+ chatIdCounter++;
+        this.isPrivate= isPrivate;
+        this.participants = new ArrayList<>();
+        this.messages= new ArrayList<>();
     }
     public void addMessage(Message message){
-        messages.add(message);
+        this.messages.add(message);
+    }
+    public void addParticipants(User user){
+        if(!participants.contains(user)){
+            participants.add(user);
+        }
+    }
+    public String getChatId(){
+        return this.chatId;
+    }
+    public List<Message> getMessages(){
+        return this.messages;
+    }
+     public List<User> getParticipants(){
+        return this.participants;
     }
 }
