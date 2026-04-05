21afbc2b34a84f6781743102340a1755a7ddca85
Asmin Deniz Kılıçoğlu <asmindeniz060@gmail.com>
2026-04-05 21:26:53 +0300

    Enhance button status logic and user management
    Refactor EventDetailsController to improve button status handling and user management.

--- eventFiles/EventDetailsController.java
+++ eventFiles/EventDetailsController.java
@@ -19,6 +19,7 @@ public class EventDetailsController {
     @FXML private Text eventNameLabel, timeLabel, locationLabel, tagsLabel;
     @FXML private Button organiserButton, joinEventButton, ignoreButton;    
     private HangoutRequest currentEvent;
+    UserManager userManager = new UserManager();
 
     public void loadDetailedData(HangoutRequest req) {
         this.currentEvent = req;
@@ -28,7 +29,7 @@ public class EventDetailsController {
         tagsLabel.setText(String.join(", ", req.getTags()));
         organiserButton.setText(req.getOrganiser().getName());
         
-        User me = LoginManager.getCurrentUser();
+        User me = userManager.getCurrentUser();
         if (req.getOrganiser().equals(me)) setupAsOrganizer();
         else setupAsParticipant(me);
     }
@@ -41,27 +42,71 @@ public class EventDetailsController {
 
     private void setupAsOrganizer() { joinEventButton.setText("DELETE"); if (ignoreButton != null) ignoreButton.setVisible(false); }
 
-    @FXML public void handleJoinEvent(ActionEvent e) { 
-        EventManager.getInstance().handleJoinEvent(currentEvent, LoginManager.getCurrentUser()); 
-        setupAsParticipant(LoginManager.getCurrentUser()); 
+    private void setupButtonStatus() {
+        User me = userManager.getCurrentUser();
+        
+        if (currentEvent.getOrganiser().equals(me)) {
+
+            joinEventButton.setText("DELETE EVENT");
+            joinEventButton.setStyle("-fx-background-color: #d32f2f; -fx-text-fill: white; -fx-background-radius: 15;");
+        } 
+        else if (currentEvent.contains(me)) {
+
+            joinEventButton.setText("LEAVE EVENT");
+            joinEventButton.setStyle("-fx-background-color: #FFA500; -fx-text-fill: white; -fx-background-radius: 15;");
+        } 
+        else if (currentEvent.isFull()) {
+
+            joinEventButton.setText("FULL");
+            joinEventButton.setDisable(true);
+            joinEventButton.setStyle("-fx-background-color: #808080; -fx-text-fill: white; -fx-background-radius: 15;");
+        } 
+        else {
+            joinEventButton.setText("JOIN EVENT");
+            joinEventButton.setDisable(false);
+            joinEventButton.setStyle("-fx-background-color: #1A365D; -fx-text-fill: white; -fx-background-radius: 15;");
+        }
+    }
+
+    @FXML 
+    public void handleJoinEvent(ActionEvent e) { 
+        User me = userManager.getCurrentUser();
+        
+        if (currentEvent.getOrganiser().equals(me)) {
+
+            EventManager.getInstance().getAllEvents().remove(currentEvent); 
+            me.getOrganizedEvents().remove(currentEvent); 
+            handleBack(e); 
+        } 
+        else if (currentEvent.contains(me)) {
+            currentEvent.getParticipants().remove(me);
+            me.getEventHistory().remove(currentEvent);
+            handleBack(e); 
+        }
+        else {
+
+            EventManager.getInstance().handleJoinEvent(currentEvent, me); 
+            setupButtonStatus(); 
+        }
     }
 
     @FXML public void handleBack(ActionEvent e) { EventController.getInstance().loadEvents(); }
     @FXML
-public void handleShowOrganiserProfile(ActionEvent event) {
-    try {
-        
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("strangerProfile.fxml"));
-        Parent root = loader.load();
+    public void handleShowOrganiserProfile(ActionEvent event) {
+        try {
+            //this part needs editing because ıdk if we have a profile view for organiser or not, if we do we can show organiser's profile when click on organiser's name
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("strangerProfile.fxml"));
+            Parent root = loader.load();
 
-        StrangerProfileController controller = loader.getController();
-        controller.setUser(currentEvent.getOrganiser()); 
+            profileViewController controller = loader.getController();
+            controller.setUser(currentEvent.getOrganiser()); 
 
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
 
-    } catch (IOException e) {
-        e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 }
