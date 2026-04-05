1c612e2d45c19d73a1d8e2e76461426a17ac2feb
Asmin Deniz Kılıçoğlu <asmindeniz060@gmail.com>
2026-04-05 16:55:24 +0300

    Refactor EventDetailsController for better user interaction
    Updated event handling and user interaction methods to improve functionality and user experience.

--- eventFiles/EventDetailsController.java
+++ eventFiles/EventDetailsController.java
@@ -1,67 +1,67 @@
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-import java.io.IOException;
+import javafx.scene.control.CheckBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class EventDetailsController {
-
-    @FXML private Label eventNameLabel;
-    @FXML private Label timeLabel;
-    @FXML private Label locationLabel;
-    @FXML private Label tagsLabel;
-    @FXML private Button organiserButton;
-    @FXML private Button joinButton;
-
+    @FXML private Text eventNameLabel, timeLabel, locationLabel, tagsLabel;
+    @FXML private Button organiserButton, joinEventButton, ignoreButton;    
     private HangoutRequest currentEvent;
 
-    public void loadDetailedData(HangoutRequest event) {
-        this.currentEvent = event;
-        eventNameLabel.setText(event.getName());
-        timeLabel.setText(event.getStartTime() + " - " + event.getEndTime());
-        locationLabel.setText(event.getLocation());
-        tagsLabel.setText(String.join(", ", event.getTags()));
+    public void loadDetailedData(HangoutRequest req) {
+        this.currentEvent = req;
+        eventNameLabel.setText(req.getName().toUpperCase());
+        timeLabel.setText(req.getStartTime() + " - " + req.getEndTime());
+        locationLabel.setText(req.getLocation());
+        tagsLabel.setText(String.join(", ", req.getTags()));
+        organiserButton.setText(req.getOrganiser().getName());
         
-        if (event.getOrganiser() != null) {
-            organiserButton.setText(event.getOrganiser().getName());
-        }
+        User me = LoginManager.getCurrentUser();
+        if (req.getOrganiser().equals(me)) setupAsOrganizer();
+        else setupAsParticipant(me);
     }
 
-    @FXML
-    public void handleShowOrganiserProfile(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Profile.fxml"));
-            Parent root = loader.load();
-            ProfileController profileController = loader.getController();
-            profileController.setUser(currentEvent.getOrganiser());
-            Stage stage = (Stage) organiserButton.getScene().getWindow();
-            stage.getScene().setRoot(root);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void setupAsParticipant(User me) {
+        if (currentEvent.contains(me)) { joinEventButton.setText("JOINED"); joinEventButton.setDisable(true); }
+        else if (currentEvent.isFull()) { joinEventButton.setText("FULL"); joinEventButton.setDisable(true); }
+        else { joinEventButton.setText("JOIN EVENT"); joinEventButton.setDisable(false); }
     }
 
-    @FXML
-    public void handleJoinEvent(ActionEvent event) {
-        User currentUser = SessionManager.getCurrentUser();
-        EventManager.getInstance().handleJoinEvent(currentEvent, currentUser);
-        joinButton.setDisable(true);
-        joinButton.setText("Joined");
+    private void setupAsOrganizer() { joinEventButton.setText("DELETE"); if (ignoreButton != null) ignoreButton.setVisible(false); }
+
+    @FXML public void handleJoinEvent(ActionEvent e) { 
+        EventManager.getInstance().handleJoinEvent(currentEvent, LoginManager.getCurrentUser()); 
+        setupAsParticipant(LoginManager.getCurrentUser()); 
     }
 
+    @FXML public void handleBack(ActionEvent e) { EventController.getInstance().loadEvents(); }
     @FXML
-    public void handleIgnoreRequest(ActionEvent event) {
-        Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-        stage.close();
+public void handleShowOrganiserProfile(ActionEvent event) {
+    try {
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("strangerProfile.fxml"));
+        Parent root = loader.load();
+
+        StrangerProfileController controller = loader.getController();
+        controller.setUser(currentEvent.getOrganiser()); 
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.getScene().setRoot(root);
+
+    } catch (IOException e) {
+        e.printStackTrace();
     }
-    
-    @FXML
-    public void handleBack(ActionEvent event) {
-        Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-        stage.close();
-    }
+}
 }
