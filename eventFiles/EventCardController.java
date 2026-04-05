fb722c3cb305388b5c7a8afa7c0a1eb5da0e5c5e
Asmin Deniz Kılıçoğlu <asmindeniz060@gmail.com>
2026-04-05 16:56:30 +0300

    Refactor EventCardController for UI changes
    Refactor EventCardController to use Text instead of TextArea and update event detail handling.

--- eventFiles/EventCardController.java
+++ eventFiles/EventCardController.java
@@ -1,47 +1,38 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
-import java.io.IOException;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class EventCardController {
 
-    @FXML private TextArea nameOfEvent;
-    @FXML private TextArea timingOfEvent;
-    @FXML private TextArea locationOfEvent;
+
+    @FXML private Text nameOfEvent;    
+    @FXML private Text timingOfEvent;  
+    @FXML private Text locationOfEvent; 
+    @FXML private Button details;       
 
     private HangoutRequest myEvent;
 
+    
     public void setData(HangoutRequest request) {
         this.myEvent = request;
-        nameOfEvent.setText(request.getName());
+        
+     
+        if (request.getName() != null) {
+            nameOfEvent.setText(request.getName().toUpperCase());
+        }
+        
         timingOfEvent.setText(request.getStartTime() + " - " + request.getEndTime());
         locationOfEvent.setText(request.getLocation());
-        
-        nameOfEvent.setEditable(false);
-        timingOfEvent.setEditable(false);
-        locationOfEvent.setEditable(false);
     }
 
     @FXML
     public void handleShowDetails(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("eventDetails.fxml"));
-            Parent root = loader.load();
 
-            EventDetailsController detailsController = loader.getController();
-            detailsController.loadDetailedData(myEvent);
-
-            Stage detailStage = new Stage();
-            detailStage.setTitle("Event Details - " + myEvent.getName());
-            detailStage.setScene(new Scene(root));
-            detailStage.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Node currentCardNode = (Node) ((Button)event.getSource()).getParent();
+        
+    
+        EventController.getInstance().replaceCardWithDetails(currentCardNode, myEvent);
     }
 }
